cb9adc68709f2ecff61a74c4a9dbcd301ab8268d
BobrovnikIlya <ilysha.bobrovnik@gmail.com>
2024-04-02 11:43:47 +0300

    Create project

---
+++ src/main/java/com/example/formofnationallibrary/FormOfNationalLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.formofnationallibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormOfNationalLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormOfNationalLibraryApplication.class, args);
+    }
+
+}
