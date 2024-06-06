d32633d538d1dcaf96142a711f7cc81b3e17ea63
BobrovnikIlya <ilysha.bobrovnik@gmail.com>
2024-06-07 02:48:27 +0300

    correct change book

--- src/main/java/com/example/formofnationallibrary/Controller/SearchController.java
+++ src/main/java/com/example/formofnationallibrary/Controller/SearchController.java
@@ -1,8 +1,6 @@
 package com.example.formofnationallibrary.Controller;
 
-import com.example.formofnationallibrary.Entities.Book;
-import com.example.formofnationallibrary.Entities.Copies;
-import com.example.formofnationallibrary.Entities.User;
+import com.example.formofnationallibrary.Entities.*;
 import com.example.formofnationallibrary.Repository.*;
 import com.example.formofnationallibrary.Service.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +19,15 @@ import org.springframework.web.servlet.ModelAndView;
 public class SearchController {
     @Autowired
     private BookService bookService;
+    @Autowired
+    private AuthorService authorService;
+    @Autowired
+    private CitiesService citiesService;
+
+    @Autowired
+    private LanguageService languageService;
+    @Autowired
+    private PublishService publishService;
     @Autowired
     private FavoriteService favoriteService;
     @Autowired
@@ -82,18 +89,52 @@ public class SearchController {
             Book book = bookRepository.findById(bookId).orElse(null);
             ModelAndView mav = new ModelAndView("ChangeBook");
             mav.addObject("book", book);
-            mav.addObject("authors", authorRepository.findAll());
-            mav.addObject("languages", languageRepository.findAll());
-            mav.addObject("publishes", publishRepository.findAll());
-            mav.addObject("id", bookId);
+            mav.addObject("id", bookId); // Передаем ID книги в представление
             return mav;
-        }
-        else{
+        } else {
             return new ModelAndView("redirect:/search");
         }
     }
     @PostMapping("/changeBook")
     public String changeBook(@ModelAttribute("book") Book updatedBook, @RequestParam Long bookId) {
+
+        Author existingAuthor = authorService.findAuthorByName(updatedBook.getAuthor().getName());
+        if (existingAuthor == null) {
+            // Если автора нет в базе данных, добавляем его
+            existingAuthor = new Author();
+            existingAuthor.setName(updatedBook.getAuthor().getName());
+            authorService.saveAuthor(existingAuthor);
+        }
+
+        Language existingLanguage = languageService.findLanguageByName(updatedBook.getLanguage().getName());
+        if (existingLanguage == null) {
+            // Если языка нет в базе данных, добавляем его
+            existingLanguage = new Language();
+            existingLanguage.setName(updatedBook.getLanguage().getName());
+            languageService.saveLanguage(existingLanguage);
+        }
+
+        Publish existingPublish = publishService.findPublishByName(updatedBook.getPublish().getName());
+        if (existingPublish == null) {
+            // Если языка нет в базе данных, добавляем его
+            existingPublish = new Publish();
+            existingPublish.setName(updatedBook.getPublish().getName());
+            publishService.savePublish(existingPublish);
+        }
+
+        Cities existingCities = citiesService.findCitiesByName(updatedBook.getLanguage().getName());
+        if (existingCities == null) {
+            // Если языка нет в базе данных, добавляем его
+            existingCities = new Cities();
+            existingCities.setName(updatedBook.getCities().getName());
+            citiesService.saveCities(existingCities);
+        }
+
+        updatedBook.setAuthor(existingAuthor);
+        updatedBook.setLanguage(existingLanguage);
+        updatedBook.setCities(existingCities);
+        updatedBook.setPublish(existingPublish);
+
         bookService.updateBook(updatedBook, bookId);
         return "redirect:/book/" + bookId;
     }
@@ -107,6 +148,7 @@ public class SearchController {
             return "redirect:/login"; // Перенаправляем на страницу входа, если пользователь не авторизован
         }
     }
+
 }
 
 
